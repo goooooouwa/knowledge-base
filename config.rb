0a854be8f37713882e9f8bcbfc09d4740063a7f2
Shunfa Xu <Shunfa Xu>
2023-03-23 10:31:50 +0800

    indent

--- config.rb
+++ config.rb
@@ -8,276 +8,276 @@ Gollum::Hook.register(:post_commit, :hook_id) do |committer, sha1|
   wiki.repo.git.push("origin", "main")
 end
 
-  # Launch Gollum using a specific git adapter. See https://github.com/gollum/gollum/wiki/Git-adapters
-  # Default: rugged
+# Launch Gollum using a specific git adapter. See https://github.com/gollum/gollum/wiki/Git-adapters
+# Default: rugged
+#
+# Equivalent to --adapter [ADAPTER]
+
+# module Gollum
+#   # to require 'my_adapter':
+#   Gollum::GIT_ADAPTER = "my"
+# end
+
+wiki_options = {
+
+  ##############################################################################
+  # Command line option equivalents
+
+  #-----------------------------------------------------------------------------
+
+  #-----------------------------------------------------------------------------
+  # Enable file uploads. If set to 'dir', Gollum will store all uploads in the
+  # /uploads/ directory in repository root. If set to 'page', Gollum will
+  # store each upload at the currently edited page.
+  # Default: false
+
+  # Equivalent to --allow-uploads dir
+  #allow_uploads: true,
+
+  # Equivalent to --allow-uploads page
+  #allow_uploads: true,
+  #per_page_uploads: true,
+
+  #-----------------------------------------------------------------------------
+  # Set the path to look for static assets.
   #
-  # Equivalent to --adapter [ADAPTER]
+  # Equivalent to --assets [PATH]
+
+  #static_assets_path: [PATH]
 
-  # module Gollum
-  #   # to require 'my_adapter':
-  #   Gollum::GIT_ADAPTER = "my"
-  # end
+  #-----------------------------------------------------------------------------
+  # Tell Gollum that the git repository should be treated as bare.
+  #
+  # Equivalent to --bare
 
-  wiki_options = {
+  #repo_is_bare: true,
 
-    ##############################################################################
-    # Command line option equivalents
+  #-----------------------------------------------------------------------------
+  # Specify the leading portion of all Gollum URLs (path info). Setting this to
+  # /wiki will make the wiki accessible under http://localhost:4567/wiki/.
+  # Default: /
+  #
+  # Equivalent to --base-path [PATH]
+
+  #base_path: [PATH],
 
-    #-----------------------------------------------------------------------------
+  #-----------------------------------------------------------------------------
+  # Enable support for annotations using CriticMarkup.
+  #
+  # Equivalent to --critic-markup
+
+  #critic_markup: true,
+
+  #-----------------------------------------------------------------------------
+  # Tell Gollum to inject custom CSS into each page. Uses custom.css from wiki
+  # root
+  #
+  # Equivalent to --css
 
-    #-----------------------------------------------------------------------------
-    # Enable file uploads. If set to 'dir', Gollum will store all uploads in the
-    # /uploads/ directory in repository root. If set to 'page', Gollum will
-    # store each upload at the currently edited page.
-    # Default: false
+  #css: true,
 
-    # Equivalent to --allow-uploads dir
-    #allow_uploads: true,
-
-    # Equivalent to --allow-uploads page
-    #allow_uploads: true,
-    #per_page_uploads: true,
+  #-----------------------------------------------------------------------------
+  # Parse and interpret emoji tags (e.g. :heart:) except when the leading colon
+  # is backslashed (e.g. \:heart:).
+  #
+  # Equivalent to --emoji
 
-    #-----------------------------------------------------------------------------
-    # Set the path to look for static assets.
-    #
-    # Equivalent to --assets [PATH]
+  #emoji: true,
+
+  #-----------------------------------------------------------------------------
+  # Tell Gollum to use the first <h1> as page title.
+  #
+  # Equivalent to --h1-title
 
-    #static_assets_path: [PATH]
+  #h1_title: true,
+
+  #-----------------------------------------------------------------------------
+  # Specify the hostname or IP address to listen on.
+  # Default: '0.0.0.0'.
+  #
+  # Equivalent to --host [HOST]
 
-    #-----------------------------------------------------------------------------
-    # Tell Gollum that the git repository should be treated as bare.
-    #
-    # Equivalent to --bare
+  # ???
+
+  #-----------------------------------------------------------------------------
+  # Launch Gollum in "console mode", with a predefined API.
+  #
+  # Equivalent to --irb
 
-    #repo_is_bare: true,
+  # ???
 
-    #-----------------------------------------------------------------------------
-    # Specify the leading portion of all Gollum URLs (path info). Setting this to
-    # /wiki will make the wiki accessible under http://localhost:4567/wiki/.
-    # Default: /
-    #
-    # Equivalent to --base-path [PATH]
+  #-----------------------------------------------------------------------------
+  # Tell Gollum to inject custom JS into each page. Uses custom.js from wiki
+  # root.
+  #
+  # Equivalent to --js
 
-    #base_path: [PATH],
+  #js: true,
 
-    #-----------------------------------------------------------------------------
-    # Enable support for annotations using CriticMarkup.
-    #
-    # Equivalent to --critic-markup
-
-    #critic_markup: true,
+  #-----------------------------------------------------------------------------
+  # Compatibility with 4.x
+  # https://github.com/gollum/gollum/wiki/5.0-release-notes#compatibility-option
+  #
+  # Internal links resolve case-insensitively, will treat spaces as hyphens, and
+  # will match the first page found with a certain filename, anywhere in the
+  # repository. Provides compatibility with Gollum 4.x.
+  #
+  # Equivalent to --lenient-tag-lookup
 
-    #-----------------------------------------------------------------------------
-    # Tell Gollum to inject custom CSS into each page. Uses custom.css from wiki
-    # root
-    #
-    # Equivalent to --css
+  #hyphened_tag_lookup: true,
+  #case_insensitive_tag_lookup: true,
+  #global_tag_lookup: true,
 
-    #css: true,
+  #-----------------------------------------------------------------------------
+  # Enables MathJax (renders mathematical equations). By default, uses the
+  # TeX-AMS-MML_HTMLorMML config with the autoload-all extension.5
+  #
+  # Equivalent to --mathjax
 
-    #-----------------------------------------------------------------------------
-    # Parse and interpret emoji tags (e.g. :heart:) except when the leading colon
-    # is backslashed (e.g. \:heart:).
-    #
-    # Equivalent to --emoji
+  #mathjax: true,
 
-    #emoji: true,
+  #-----------------------------------------------------------------------------
+  # Specify path to a custom MathJax configuration.
+  # Default: mathjax.config.js file from repository root.
+  #
+  # Equivalent to --mathjax-config [FILE]
 
-    #-----------------------------------------------------------------------------
-    # Tell Gollum to use the first <h1> as page title.
-    #
-    # Equivalent to --h1-title
+  #mathjax_config: [FILE],
 
-    #h1_title: true,
+  #-----------------------------------------------------------------------------
+  # Do not render metadata tables in pages.
+  #
+  # Equivalent to --no-display-metadata
 
-    #-----------------------------------------------------------------------------
-    # Specify the hostname or IP address to listen on.
-    # Default: '0.0.0.0'.
-    #
-    # Equivalent to --host [HOST]
+  #display_metadata: false,
 
-    # ???
+  #-----------------------------------------------------------------------------
+  # Disable the feature of editing pages.
+  # Default: true
+  #
+  # Equivalent to --no-edit
 
-    #-----------------------------------------------------------------------------
-    # Launch Gollum in "console mode", with a predefined API.
-    #
-    # Equivalent to --irb
+  #allow_editing: false,
 
-    # ???
+  #-----------------------------------------------------------------------------
+  # Specify the subdirectory for all pages. If set, Gollum will only serve pages
+  # from this directory and its subdirectories.
+  # Default: repository root.
+  #
+  # Equivalent to --page-file-dir [PATH]
 
-    #-----------------------------------------------------------------------------
-    # Tell Gollum to inject custom JS into each page. Uses custom.js from wiki
-    # root.
-    #
-    # Equivalent to --js
+  #page_file_dir: [PATH],
 
-    #js: true,
+  #-----------------------------------------------------------------------------
+  # Follow pages across renames in the History view.
+  # Default: true.
 
-    #-----------------------------------------------------------------------------
-    # Compatibility with 4.x
-    # https://github.com/gollum/gollum/wiki/5.0-release-notes#compatibility-option
-    #
-    # Internal links resolve case-insensitively, will treat spaces as hyphens, and
-    # will match the first page found with a certain filename, anywhere in the
-    # repository. Provides compatibility with Gollum 4.x.
-    #
-    # Equivalent to --lenient-tag-lookup
+  # Equivalent to --follow-renames
+  #follow_renames: true,
 
-    #hyphened_tag_lookup: true,
-    #case_insensitive_tag_lookup: true,
-    #global_tag_lookup: true,
+  # Equivalent to --no-follow-renames
+  #follow_renames: false,
 
-    #-----------------------------------------------------------------------------
-    # Enables MathJax (renders mathematical equations). By default, uses the
-    # TeX-AMS-MML_HTMLorMML config with the autoload-all extension.5
-    #
-    # Equivalent to --mathjax
+  #-----------------------------------------------------------------------------
+  # Specify the port to bind Gollum with.
+  # Default: 4567.
+  #
+  # Equivalent to --port [PORT]
 
-    #mathjax: true,
+  # ???
 
-    #-----------------------------------------------------------------------------
-    # Specify path to a custom MathJax configuration.
-    # Default: mathjax.config.js file from repository root.
-    #
-    # Equivalent to --mathjax-config [FILE]
+  #-----------------------------------------------------------------------------
+  # Specify the git branch to serve.
+  # Default: master.
+  #
+  # Equivalent to --ref [REF]
 
-    #mathjax_config: [FILE],
+  ref: 'main',
 
-    #-----------------------------------------------------------------------------
-    # Do not render metadata tables in pages.
-    #
-    # Equivalent to --no-display-metadata
+  #-----------------------------------------------------------------------------
+  # Use static assets.
+  # Defaults to false in development/test, true in
+  # production/staging.
 
-    #display_metadata: false,
+  # Equivalent to --static
+  #static: true,
 
-    #-----------------------------------------------------------------------------
-    # Disable the feature of editing pages.
-    # Default: true
-    #
-    # Equivalent to --no-edit
+  # Equivalent to --no-static
+  #static: false,
 
-    #allow_editing: false,
+  #-----------------------------------------------------------------------------
+  # Specify custom mustache template directory.
+  #
+  # Equivalent to --template-dir [PATH]
 
-    #-----------------------------------------------------------------------------
-    # Specify the subdirectory for all pages. If set, Gollum will only serve pages
-    # from this directory and its subdirectories.
-    # Default: repository root.
-    #
-    # Equivalent to --page-file-dir [PATH]
+  #template_dir: [PATH],
 
-    #page_file_dir: [PATH],
+  #-----------------------------------------------------------------------------
+  # Use _Template in root as a template for new pages. Must be committed.
+  #
+  # Equivalent to --template-page
 
-    #-----------------------------------------------------------------------------
-    # Follow pages across renames in the History view.
-    # Default: true.
+  # template_page: true,
 
-    # Equivalent to --follow-renames
-    #follow_renames: true,
+  #-----------------------------------------------------------------------------
+  # Tell Gollum to use specific user icons for history view. Can be set to
+  # gravatar, identicon or none.
+  # Default: none.
+  #
+  # Equivalent to --user-icons [MODE]
 
-    # Equivalent to --no-follow-renames
-    #follow_renames: false,
+  #user_icons: [MODE],
 
-    #-----------------------------------------------------------------------------
-    # Specify the port to bind Gollum with.
-    # Default: 4567.
-    #
-    # Equivalent to --port [PORT]
+  ##############################################################################
+  # Metadata (front matter)
 
-    # ???
+  #-----------------------------------------------------------------------------
+  # Header counting
+  # Default: false
+  # Can also be a pre-defined counter. See
+  # https://www.w3.org/TR/css-counter-styles-3/#predefined-counters
 
-    #-----------------------------------------------------------------------------
-    # Specify the git branch to serve.
-    # Default: master.
-    #
-    # Equivalent to --ref [REF]
+  #header_enum: false,
+  #header_enum: 'decimal-leading-zero',
 
-    ref: 'main',
+  #-----------------------------------------------------------------------------
+  # Global metadata. Arbitrary metadata that will be applied to each page.
 
-    #-----------------------------------------------------------------------------
-    # Use static assets.
-    # Defaults to false in development/test, true in
-    # production/staging.
+  #metadata: {
+  #  monkeyboys: 'are loose in the facility'
+  #},
 
-    # Equivalent to --static
-    #static: true,
+  ##############################################################################
+  # Tips
 
-    # Equivalent to --no-static
-    #static: false,
+  #-----------------------------------------------------------------------------
+  # Force table of contents tag ([[_TOC_]]) into each page.
+  # ??? How to set the level in the configuration file?
+  # https://github.com/gollum/gollum/wiki#table-of-contents-toc-tag
+  # Default: false
 
-    #-----------------------------------------------------------------------------
-    # Specify custom mustache template directory.
-    #
-    # Equivalent to --template-dir [PATH]
+  #universal_toc: false,
 
-    #template_dir: [PATH],
+  #-----------------------------------------------------------------------------
+  # Move the sidebar to the left of the page
+  # https://github.com/gollum/gollum/issues/1450#issuecomment-599124384
 
-    #-----------------------------------------------------------------------------
-    # Use _Template in root as a template for new pages. Must be committed.
-    #
-    # Equivalent to --template-page
+  # sidebar: :left,
 
-    # template_page: true,
+  #-----------------------------------------------------------------------------
+  # Change the home page name
+  # https://github.com/gollum/gollum/issues/1569#issuecomment-633033895
 
-    #-----------------------------------------------------------------------------
-    # Tell Gollum to use specific user icons for history view. Can be set to
-    # gravatar, identicon or none.
-    # Default: none.
-    #
-    # Equivalent to --user-icons [MODE]
+  #index_page: 'index',
 
-    #user_icons: [MODE],
+  #-----------------------------------------------------------------------------
+  # Change the number of changes in the rss feed
 
-    ##############################################################################
-    # Metadata (front matter)
+  #pagination_count: 15
+}
 
-    #-----------------------------------------------------------------------------
-    # Header counting
-    # Default: false
-    # Can also be a pre-defined counter. See
-    # https://www.w3.org/TR/css-counter-styles-3/#predefined-counters
+#-------------------------------------------------------------------------------
+# Change default markup
+#Precious::App.set(:default_markup, :asciidoc)
 
-    #header_enum: false,
-    #header_enum: 'decimal-leading-zero',
-
-    #-----------------------------------------------------------------------------
-    # Global metadata. Arbitrary metadata that will be applied to each page.
-
-    #metadata: {
-    #  monkeyboys: 'are loose in the facility'
-    #},
-
-    ##############################################################################
-    # Tips
-
-    #-----------------------------------------------------------------------------
-    # Force table of contents tag ([[_TOC_]]) into each page.
-    # ??? How to set the level in the configuration file?
-    # https://github.com/gollum/gollum/wiki#table-of-contents-toc-tag
-    # Default: false
-
-    #universal_toc: false,
-
-    #-----------------------------------------------------------------------------
-    # Move the sidebar to the left of the page
-    # https://github.com/gollum/gollum/issues/1450#issuecomment-599124384
-
-    # sidebar: :left,
-
-    #-----------------------------------------------------------------------------
-    # Change the home page name
-    # https://github.com/gollum/gollum/issues/1569#issuecomment-633033895
-
-    #index_page: 'index',
-
-    #-----------------------------------------------------------------------------
-    # Change the number of changes in the rss feed
-
-    #pagination_count: 15
-  }
-
-  #-------------------------------------------------------------------------------
-  # Change default markup
-  #Precious::App.set(:default_markup, :asciidoc)
-
-  Precious::App.set(:wiki_options, wiki_options)
+Precious::App.set(:wiki_options, wiki_options)
