7725392dbd81e62f943dbe7da68d524afea30e1e
Shunfa Xu <Shunfa Xu>
2023-03-23 20:08:32 +0800

    load dotenv

--- config.rb
+++ config.rb
@@ -1,6 +1,7 @@
 require 'gollum/app'
 require 'omnigollum'
 require 'omniauth/strategies/github'
+require 'dotenv/load'
 
 # Per https://github.com/gollum/gollum-lib/issues/12
 Gollum::Hook.register(:post_commit, :hook_id) do |committer, sha1|
